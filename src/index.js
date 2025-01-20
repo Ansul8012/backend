28783a48c03f942196be3178566caa0f85344fc4
ansul singh <ansulsingh@ansuls-MacBook-Air.local>
2025-01-20 19:27:26 +0530

    setup project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import mongoose from "mongoose";
 
-import ConnectDB from "./db";
+import dotenv from "dotenv"
+import ConnectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
 
 
 ConnectDB();
