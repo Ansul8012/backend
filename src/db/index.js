a228be6019c59a5b90ca76eb266accd9238d3fea
ansul singh <ansulsingh@ansuls-MacBook-Air.local>
2025-01-20 18:48:40 +0530

    done

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+import {DB_NAME} from "../constants.js"
+
+const ConnectDB=async ()=>{
+    try {
+        const connectInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`/n MONGODB CONNECTED !!! DB HOST: ${connectInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGO DB is Connected",error);
+        process.exit(1);
+    }
+}
+
+export default ConnectDB;
