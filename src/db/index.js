3ff6f4115a6087b12f077e338f7a617c46fa363c
ansul singh <ansulsingh@ansuls-MacBook-Air.local>
2025-01-20 20:50:46 +0530

    set up

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import {DB_NAME} from "../constants.js"
 const ConnectDB=async ()=>{
     try {
         const connectInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`/n MONGODB CONNECTED !!! DB HOST: ${connectInstance.connection.host}`);
+        console.log(`\n MONGODB CONNECTED !!! DB HOST: ${connectInstance.connection.host}`);
     } catch (error) {
         console.log("MONGO DB is Connected",error);
         process.exit(1);
