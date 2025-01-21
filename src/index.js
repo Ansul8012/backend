200a5d0fad9fd7a8800d1c257eca25899ed1f404
ansul singh <ansulsingh@ansuls-MacBook-Air.local>
2025-01-21 12:19:52 +0530

    setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv"
 import ConnectDB from "./db/index.js";
 
+
 dotenv.config({
     path:'./env'
 })
 
 
-ConnectDB();
+ConnectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+    })
+})
+.catch((err)=>{
+    console.log('Mongo DB failed ERROR:',err);
+})
